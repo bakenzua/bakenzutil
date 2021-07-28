5d7fb198aace1babb8a87a60ed2864d1e94fa46f
Barney <barnaby.sanderson@gstt.nhs.uk>
2021-07-28 16:35:34 +0100

    ?edits to medianIQRstring

--- R/medianIqrString.R
+++ R/medianIqrString.R
@@ -2,13 +2,13 @@
 #'
 #' @param x A vector of numeric values to be evaluated
 #' @param na.rm logical; if true, any NA and NaN's are removed from x before the quantiles are computed.
-#' @param probs numeric vector of probabilities with values in [0,1], default 0.25 and 0.5, only first two probs will be reported as range
+#' @param probs numeric vector of probabilities with values in [0,1], default 0.25 and 0.75, only first two probs will be reported as range
 #' @param digits numeric number of digits for rounding, default=2
 #'
 #' @return result_string
 #' @export
 #'
-medianIqrString <- function(x, na.rm=T, probs=c(0.25, 0.5), digits=2) {
+medianIqrString <- function(x, na.rm=T, probs=c(0.25, 0.75), digits=2) {
 
   result_string <- paste0(
     round(median(x, na.rm = na.rm), digits = digits),
