1f565936e2cf266f177df5819e072fa223861a40
bsanderson <barnaby.sanderson@gstt.nhs.uk>
2022-03-16 13:30:04 +0000

    tweaks to theme_clean()

--- R/theme_bakenzua_clean.R
+++ R/theme_bakenzua_clean.R
@@ -8,42 +8,56 @@
 #' @examples
 #' ggplot2::theme_set(theme_bakenzua_clean())
 theme_bakenzua_clean <- function(...) {
+
+  font <- "sans"
+
   theme_minimal() +
     theme(
       plot.background = element_rect(fill = "white", color = NA),
       panel.grid.minor = element_blank(),
       axis.line.x = element_line(size = 0.5),
       plot.title = element_text(
+        color = '#222222',
         size = rel(2),
         face = "bold",
-        family = 'sans'
+        family = font
       ),
       plot.subtitle = element_text(
+        color = '#222222',
         size = rel(1.2),
-        family = 'sans'
+        family = font
       ),
       axis.title = element_text(
+        color = '#222222',
         size = rel(1.2),
         face = "bold",
-        family = 'sans'
+        family = font
       ),
       axis.title.y = element_text(margin = margin(r=20, l=10)),
       axis.title.x = element_text(margin = margin(t=20, b=10)),
       # axis.line.x = element_line(size = 1),
       axis.text = element_text(
+        color = '#222222',
         size = rel(1),
-        family = 'sans'
+        family = font
       ),
-      strip.text = element_text(face = "bold", size = rel(1), hjust = 0),
+      strip.text = element_text(
+        color = '#222222',
+        face = "bold",
+        size = rel(1),
+        hjust = 0
+      ),
       strip.background = element_rect(fill = "grey80", color = NA),
       legend.title = element_text(
+        color = '#222222',
         size = rel(1.6),
         face = "bold",
-        family = 'sans'
+        family = font
       ),
       legend.text = element_text(
+        color = '#222222',
         size = rel(1.2),
-        family = 'sans'
+        family = font
       ),
       # Additional settings passed to theme()
       ...
